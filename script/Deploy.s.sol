f52d5e241ef51a406cee4a93932ae87ebfadf71f
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-02 16:51:20 +0400

    fix: expand deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,15 +6,22 @@ import {HeartbeatAVS} from "../src/HeartbeatAVS.sol";
 import {MockEigenSlasher} from "../src/mocks/MockEigenSlasher.sol";
 
 contract DeployHeartbeatAVS is Script {
-    function run() external {
-        vm.startBroadcast();
+    function run() external returns (HeartbeatAVS, MockEigenSlasher) {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address operatorToRegister = vm.envAddress("OPERATOR_ADDRESS");
+
+        vm.startBroadcast(deployerPrivateKey);
 
         MockEigenSlasher slasher = new MockEigenSlasher();
-        HeartbeatAVS avs = new HeartbeatAVS(address(slasher));
-
-        console.log("HeartbeatAVS deployed to:", address(avs));
         console.log("MockEigenSlasher deployed to:", address(slasher));
 
+        HeartbeatAVS avs = new HeartbeatAVS(address(slasher));
+        console.log("HeartbeatAVS deployed to:", address(avs));
+
+        console.log("Registering Operator:", operatorToRegister);
+        avs.registerOperator(operatorToRegister);
+
         vm.stopBroadcast();
+        return (avs, slasher);
     }
 }
