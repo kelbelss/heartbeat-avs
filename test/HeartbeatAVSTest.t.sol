040479777b4364dfab7c8e9a3c5181a87dd7afac
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-02 12:10:13 +0400

    chore: write tests for current set up

--- test/HeartbeatAVSTest.t.sol
+++ test/HeartbeatAVSTest.t.sol
@@ -2,7 +2,75 @@
 pragma solidity 0.8.29;
 
 import {Test, console} from "forge-std/Test.sol";
+import {HeartbeatAVS} from "../src/HeartbeatAVS.sol";
+import {MockEigenSlasher} from "../src/mocks/MockEigenSlasher.sol";
 
 contract HeartbeatAVSTest is Test {
-    function setUp() public {}
+    HeartbeatAVS internal avs;
+    MockEigenSlasher internal slasher;
+
+    address operator = makeAddr("operator");
+    address nonOperator = makeAddr("nonOperator");
+    uint256 interval = 30;
+    uint256 grace = 10;
+
+    function setUp() public {
+        slasher = new MockEigenSlasher();
+        avs = new HeartbeatAVS(address(slasher));
+
+        // reigister an operator
+        vm.prank(address(this));
+        avs.registerOperator(operator);
+    }
+
+    function testPingSuccess() public {
+        vm.prank(operator);
+        avs.ping("System nominal");
+
+        assertEq(avs.lastPing(operator), block.timestamp);
+        console.log("Pinged operator:", operator);
+        console.log("Pinged timestamp:", block.timestamp);
+    }
+
+    function testPingRevertsIfNotRegistered() public {
+        vm.expectRevert(HeartbeatAVS.NotAnOperator.selector);
+        vm.prank(nonOperator);
+        avs.ping("Trying to ping");
+        console.log("Pinged non-operator:", nonOperator);
+    }
+
+    function testCannotSlashBeforeGracePeriod() public {
+        vm.prank(operator);
+        avs.ping("OK");
+
+        uint256 expected = block.timestamp + interval + grace;
+
+        // warp just before slash window
+        vm.warp(expected - 1);
+
+        vm.expectRevert(abi.encodeWithSelector(HeartbeatAVS.InvalidSlash.selector, expected));
+        avs.slash(operator);
+    }
+
+    function testCanSlashAfterGracePeriod() public {
+        vm.prank(operator);
+        avs.ping("Initial");
+
+        vm.warp(block.timestamp + interval + grace + 1);
+
+        avs.slash(operator);
+    }
+
+    function testEventsEmit() public {
+        vm.prank(operator);
+        vm.expectEmit(true, true, false, true);
+        emit HeartbeatAVS.Pinged(operator, block.timestamp, "Check");
+        avs.ping("Check");
+
+        vm.warp(block.timestamp + interval + grace + 1);
+
+        vm.expectEmit(true, true, false, true);
+        emit HeartbeatAVS.Slashed(operator, block.timestamp - (interval + grace + 1));
+        avs.slash(operator);
+    }
 }
