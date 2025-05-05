edd449733274d543a8bd904bc782d771a7c6e182
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-05 15:56:53 +0400

    refactor: remove files that are not used

--- script/PingOperator.s.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.29;
-
-import {Script} from "forge-std/Script.sol";
-
-contract SlashOperator is Script {
-    function run() external {}
-}
