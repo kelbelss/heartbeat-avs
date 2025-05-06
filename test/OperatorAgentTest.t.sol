d952d640f20324f90a7e481eff9dc43879ad0dec
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-05-06 15:43:59 +0400

    Delete OperatorAgentTest.t.sol

--- test/OperatorAgentTest.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.29;
-
-import {Test, console} from "forge-std/Test.sol";
-
-contract OperatorAgentTest is Test {
-    function setUp() public {}
-}
